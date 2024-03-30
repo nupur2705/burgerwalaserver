2fc31142032c7180c36b8d4b8dfab7ed02a3b561
Nupur <nupursatpathy1@gmail.com>
2024-03-30 19:19:06 +0530

    commit

---
+++ app.js
@@ -0,0 +1,36 @@
+import express, { urlencoded } from "express";
+import dotenv from "dotenv";
+import {connectPassport} from "./utils/Provider.js";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+const app=express();
+export default app;
+dotenv.config({
+    path:"./config/config.env",
+});
+
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false,
+}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(urlencoded({
+    extended:true,
+}))
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+connectPassport();
+
+import userRoute from "./routes/user.js";
+import orderRoute from "./routes/order.js";
+app.use("/api/v1",userRoute);
+app.use("api/v1",orderRoute);
+
+app.use(errorMiddleware);
