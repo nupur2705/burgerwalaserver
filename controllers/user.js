68afee73318cd2c4b8d12804b5c0bf26321576d9
Nupur <nupursatpathy1@gmail.com>
2024-04-05 11:08:53 +0530

    changes2

--- controllers/user.js
+++ controllers/user.js
@@ -7,15 +7,20 @@ res.status(200).json({
     user:req.user,
 });
 };
-export const logout=(req,res,next)=>{
-    req.session.destroy((err)=>{
-        if (err) return next(err);
-        res.clearCookie("connect.sid");
-        res.status(200).json({
-            message:"Logged Out",
-        });
+export const logout = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) return next(err);
+    res.clearCookie("connect.sid", {
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
     });
+    res.status(200).json({
+      message: "Logged Out",
+    });
+  });
 };
+
 
 export const getAdminUsers = asyncError(async (req, res, next) => {
   const users = await User.find({});
